0d6724b7b8d341d8db267f97916563d325e5361f
Mars Willilams <mars.williams@airbnb.com>
2017-09-28 17:15:56 -0700

    using vanilla deserialization for now to reduce debugging time

--- app/src/main/java/com/marswilliams/apps/sweettweets/models/Tweet.java
+++ app/src/main/java/com/marswilliams/apps/sweettweets/models/Tweet.java
@@ -1,27 +1,42 @@
 package com.marswilliams.apps.sweettweets.models;
 
-import org.parceler.Parcel;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by mars_williams on 9/28/17.
  */
 
-@Parcel(analyze={Tweet.class})
+//@Parcel(analyze={Tweet.class})
 public class Tweet {
-    private String body;
-    private long iud;
-    private String createdAt;
-    private User user;
+//    private String body;
+//    private long iud;
+//    private String createdAt;
+//    private User user;
+//
+//    // empty constructor needed by the Parceler library
+//    public Tweet() {
+//    }
+//
+//    public Tweet(String body, long iud, String createdAt, User user) {
+//        this.body = body;
+//        this.iud = iud;
+//        this.createdAt = createdAt;
+//        this.user = new User();
+//    }
 
-    // empty constructor needed by the Parceler library
-    public Tweet() {
+    public String body;
+    public long iud;
+    public String createdAt;
+    public User user;
+
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("body");
+        tweet.iud = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
     }
-
-    public Tweet(String body, long iud, String createdAt, User user) {
-        this.body = body;
-        this.iud = iud;
-        this.createdAt = createdAt;
-        this.user = new User();
-    }
-
 }
--- app/src/main/java/com/marswilliams/apps/sweettweets/models/User.java
+++ app/src/main/java/com/marswilliams/apps/sweettweets/models/User.java
@@ -1,26 +1,43 @@
 package com.marswilliams.apps.sweettweets.models;
 
-import org.parceler.Parcel;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by mars_williams on 9/28/17.
  */
 
-@Parcel(analyze={User.class})
+//@Parcel(analyze={User.class})
 public class User {
-    private String name;
-    private long iud;
-    private String screenName;
-    private String profileImageUrl;
+//    private String name;
+//    private long iud;
+//    private String screenName;
+//    private String profileImageUrl;
+//
+//    // empty constructor needed by the Parceler library
+//    public User() {
+//    }
+//
+//    public User(String name, long iud, String screenName, String profileImageUrl) {
+//        this.name = name;
+//        this.iud = iud;
+//        this.screenName = screenName;
+//        this.profileImageUrl = profileImageUrl;
+//    }
 
-    // empty constructor needed by the Parceler library
-    public User() {
-    }
+    public String name;
+    public long iud;
+    public String screenName;
+    public String profileImageUrl;
 
-    public User(String name, long iud, String screenName, String profileImageUrl) {
-        this.name = name;
-        this.iud = iud;
-        this.screenName = screenName;
-        this.profileImageUrl = profileImageUrl;
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+
+        User user = new User();
+        user.name = jsonObject.getString("name");
+        user.iud = jsonObject.getLong("id");
+        user.screenName = jsonObject.getString("screen_name");
+        user.profileImageUrl = jsonObject.getString("profile_image_url");
+
+        return user;
     }
 }
