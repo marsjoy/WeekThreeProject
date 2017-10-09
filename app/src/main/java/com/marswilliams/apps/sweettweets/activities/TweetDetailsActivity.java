b41310cf307dded3d99e9c1322d56851dc295d54
Mars Willilams <mars.williams@airbnb.com>
2017-10-09 04:30:25 -0700

    details page

--- app/src/main/java/com/marswilliams/apps/sweettweets/activities/TweetDetailsActivity.java
+++ app/src/main/java/com/marswilliams/apps/sweettweets/activities/TweetDetailsActivity.java
@@ -1,30 +1,79 @@
 package com.marswilliams.apps.sweettweets.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.marswilliams.apps.sweettweets.R;
 import com.marswilliams.apps.sweettweets.models.Tweet;
+import com.marswilliams.apps.sweettweets.networking.TwitterClient;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+import static com.marswilliams.apps.sweettweets.R.id.tvBodyDetails;
+import static com.marswilliams.apps.sweettweets.R.id.tvCreatedAtDetails;
+import static com.marswilliams.apps.sweettweets.activities.TimelineActivity.TWEET_POSITION;
+
 
 /**
  * Created by mars_williams on 10/6/17.
  */
 
 public class TweetDetailsActivity extends AppCompatActivity {
-    public static final String EXTRA_TWEET = "TWEET";
+    @BindView(R.id.ivProfileImageDetails)
+    ImageView ivProfileImage;
+    @BindView(R.id.tvUserNameDetails)
+    TextView tvUserName;
+    @BindView(R.id.tvScreenNameDetails)
+    TextView tvScreenName;
+    @BindView(tvCreatedAtDetails)
+    TextView tvCreatedAt;
+    @BindView(tvBodyDetails)
+    TextView tvBody;
     Tweet tweet;
+    int position;
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet_details);
-
         ButterKnife.bind(this);
 
-        tweet = Parcels.unwrap(getIntent().getExtras().getParcelable(EXTRA_TWEET));
+        tweet = Parcels.unwrap(getIntent().getParcelableExtra(Tweet.class.getSimpleName()));
+        position = getIntent().getIntExtra(TWEET_POSITION, 0);
+
+        populateDetails();
+    }
+
+    public void populateDetails() {
+//      Populate the views according to this data
+        tvUserName.setText(tweet.getUser().getName());
+        tvBody.setText(tweet.getBody());
+        tvScreenName.setText(getString(R.string.formatted_user_screen_name, tweet.getUser().getScreenName()));
+        tvCreatedAt.setText(tweet.getRelativeCreatedAt());
+
+        // Load the profile image
+        Glide.with(this)
+                .load(tweet.getUser().getProfileImageUrl())
+                .bitmapTransform(new RoundedCornersTransformation(this, 25, 0))
+                .into(ivProfileImage);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Return to the calling activity, send the new tweet and position
+        Intent intent = new Intent();
+        intent.putExtra(Tweet.class.getSimpleName(), Parcels.wrap(tweet));
+        intent.putExtra(TWEET_POSITION, position);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/marswilliams/apps/sweettweets/adapters/TweetAdapter.java
+++ app/src/main/java/com/marswilliams/apps/sweettweets/adapters/TweetAdapter.java
@@ -11,10 +11,8 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.marswilliams.apps.sweettweets.R;
-import com.marswilliams.apps.sweettweets.TwitterApplication;
 import com.marswilliams.apps.sweettweets.activities.TweetDetailsActivity;
 import com.marswilliams.apps.sweettweets.models.Tweet;
-import com.marswilliams.apps.sweettweets.networking.TwitterClient;
 
 import org.parceler.Parcels;
 
@@ -24,6 +22,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
+import static com.marswilliams.apps.sweettweets.activities.TimelineActivity.TWEET_POSITION;
+
 /**
  * Created by mars_williams on 9/29/17.
  */
@@ -32,9 +32,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
     // pass in the tweets array to the constructor
 
     private static List<Tweet> mTweets;
-    public TwitterClient client;
     Context context;
-    Tweet tweet;
 
     // Pass in the Tweets array in the constructor
     public TweetAdapter(List<Tweet> tweets) {
@@ -62,9 +60,10 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
 
     // bind the values based on the position of the element
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        final Context context = holder.rootView.getContext();
         // get the data according to the position
-        tweet = mTweets.get(position);
+        final Tweet tweet = mTweets.get(position);
         Glide.with(context)
                 .load(tweet.getUser().getProfileImageUrl())
                 .placeholder(R.drawable.ic_profile_image_placeholder)
@@ -79,10 +78,17 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
         holder.tvCreatedAt.setText(tweet.getRelativeCreatedAt());
         holder.rootView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openTweetDetails(context, tweet);
+            public void onClick(View view) {
+                TweetAdapter.this.openTweetDetails(context, tweet, holder.getAdapterPosition());
             }
         });
+    }
+
+    public void openTweetDetails(Context context, Tweet tweet, int position) {
+        Intent intent = new Intent(context, TweetDetailsActivity.class);
+        intent.putExtra(Tweet.class.getSimpleName(), Parcels.wrap(tweet));
+        intent.putExtra(TWEET_POSITION, position);
+        context.startActivity(intent);
     }
 
     @Override
@@ -123,14 +129,6 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
             super(itemView);
 
             ButterKnife.bind(this, itemView);
-
-            client = TwitterApplication.getRestClient();
         }
-    }
-
-    public void openTweetDetails(Context context, Tweet tweet){
-        Intent intent = new Intent(context, TweetDetailsActivity.class);
-        intent.putExtra(TweetDetailsActivity.EXTRA_TWEET, Parcels.wrap(tweet));
-        context.startActivity(intent);
     }
 }
